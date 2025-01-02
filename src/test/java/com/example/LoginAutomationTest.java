7d9eb70b843ed8b7ecb4db662e30d07dc6e1ffd7
Akash200325 <142168006+Akash200325@users.noreply.github.com>
2025-01-02 20:24:34 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/LoginAutomationTest.java
+++ src/test/java/com/example/LoginAutomationTest.java
@@ -5,15 +5,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginAutomationTest {
 
     @Test
     public void testLogin() {
-        // Set up WebDriverManager to automatically handle ChromeDriver
-        WebDriverManager.chromedriver().setup();
+        // Manually set the path to ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\akash\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        // Initialize ChromeDriver
         WebDriver driver = new ChromeDriver();
 
         try {
