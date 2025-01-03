e28c386401de3e3df5c1d176b480c8258c0a316b
Akash200325 <142168006+Akash200325@users.noreply.github.com>
2025-01-03 10:31:53 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -25,38 +25,56 @@ public class App {
     static class LoginHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            if ("GET".equals(exchange.getRequestMethod())) {
-                String htmlResponse = """
-                        <!DOCTYPE html>
-                        <html>
-                        <head>
-                            <title>Login Page</title>
-                        </head>
-                        <body>
-                            <h1>Login</h1>
-                            <form action="/login" method="post">
-                                <label for="username">Username:</label>
-                                <input type="text" id="username" name="username"><br><br>
-                                <label for="password">Password:</label>
-                                <input type="password" id="password" name="password"><br><br>
-                                <button type="submit">Submit</button>
-                            </form>
-                        </body>
-                        </html>
-                        """;
+            String response;
+            switch (exchange.getRequestMethod()) {
+                case "GET":
+                    response = getLoginForm();
+                    sendResponse(exchange, response, "text/html");
+                    break;
 
-                exchange.getResponseHeaders().set("Content-Type", "text/html");
-                exchange.sendResponseHeaders(200, htmlResponse.getBytes().length);
-                OutputStream os = exchange.getResponseBody();
-                os.write(htmlResponse.getBytes());
-                os.close();
-            } else if ("POST".equals(exchange.getRequestMethod())) {
-                // Process login submission (dummy handling for demonstration)
-                String response = "Login successful!";
-                exchange.sendResponseHeaders(200, response.getBytes().length);
-                OutputStream os = exchange.getResponseBody();
+                case "POST":
+                    // Process login submission (dummy handling for demonstration)
+                    response = "Login successful!";
+                    sendResponse(exchange, response, "text/plain");
+                    break;
+
+                default:
+                    // Handle unsupported methods
+                    response = "Method Not Allowed";
+                    exchange.sendResponseHeaders(405, response.length());
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                    break;
+            }
+        }
+
+        private String getLoginForm() {
+            return """
+                    <!DOCTYPE html>
+                    <html>
+                    <head>
+                        <title>Login Page</title>
+                    </head>
+                    <body>
+                        <h1>Login</h1>
+                        <form action="/login" method="post">
+                            <label for="username">Username:</label>
+                            <input type="text" id="username" name="username"><br><br>
+                            <label for="password">Password:</label>
+                            <input type="password" id="password" name="password"><br><br>
+                            <button type="submit">Submit</button>
+                        </form>
+                    </body>
+                    </html>
+                    """;
+        }
+
+        private void sendResponse(HttpExchange exchange, String response, String contentType) throws IOException {
+            exchange.getResponseHeaders().set("Content-Type", contentType);
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response.getBytes());
-                os.close();
             }
         }
     }
